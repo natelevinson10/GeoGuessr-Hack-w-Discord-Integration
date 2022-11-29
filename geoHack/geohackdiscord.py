c4bcdaadfd5793b917fe756c00394ffdda134528
natelevinson10 <78764811+natelevinson10@users.noreply.github.com>
2022-11-29 13:36:37 -0500

    Update geohackdiscord.py

--- geoHack/geohackdiscord.py
+++ geoHack/geohackdiscord.py
@@ -3,8 +3,10 @@ from discord.ext import commands
 from discord_webhook import DiscordWebhook, DiscordEmbed
 from discord.ext.commands import bot
 
+#request made to geoguessr API
 def getLocation(gameID):
     response = requests.get(f"https://www.geoguessr.com/api/v3/games/{gameID}")
+    #convert response to json object, parse lat and lng coordinates
     obj = response.json()
     round = obj['round']
     lat = (obj["rounds"][round - 1]["lat"]) 
@@ -14,10 +16,10 @@ def getLocation(gameID):
     headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
     }
-
+    #POST request passing coords into a coordinate formatter
     response2 = requests.get("https://api.opencagedata.com/geocode/v1/json?q=" + str(lat) + "+" + str(lng) + "&key=03c48dae07364cabb7f121d8c1519492", headers=  headers)
     obj2 = response2.json()
-
+    #parse and return final formatted location
     formatted = (obj2["results"][0]["formatted"])
     return formatted
 
@@ -25,10 +27,12 @@ def getLocation(gameID):
 bot = commands.Bot(command_prefix="!")
 client = discord.Client()
 
+#make sure bot is online
 @bot.event
 async def on_ready():
     print("Everything's all ready to go~")
 
+#send discord embed w/ formatted location to desired channel
 @bot.command()
 async def find(ctx, gameID):
     webhook = DiscordWebhook(url="DISCORD_WEBHOOK_URL")
